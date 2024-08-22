284d19a8258cc0157a9c90ebfb3074c60f25d3a8
sarzapalo <sarzapalosupanta@gmail.com>
2024-08-21 21:46:43 -0500

    feat: add models & controllers

---
+++ Controllers/AccesoController.cs
@@ -0,0 +1,45 @@
+﻿using RetoTecnicoAjinomoto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RetoTecnicoAjinomoto.Controllers
+{
+    public class AccesoController : Controller
+    {
+        public ActionResult Login(Login oLogin)
+        {
+            if (!string.IsNullOrWhiteSpace(oLogin.Usuario) && !string.IsNullOrWhiteSpace(oLogin.Contrasena))
+            {
+                using (TareasDbModels context = new TareasDbModels())
+                {
+                    oLogin.Contrasena = Base64Encode(oLogin.Contrasena);
+                    var ListaUsuarioEntity = context.Usuarios.Where(x => x.Correo == oLogin.Usuario && x.Contrasena == oLogin.Contrasena).ToList();
+                    if (ListaUsuarioEntity.Count == 0)
+                    {
+                        ViewData["Respuesta"] = "Usuario no encontrado";
+                        return View();
+
+                    }
+                    else
+                    {
+                        return View("~/Views/Tareas/Index.cshtml");
+                    }
+                }
+            }
+            else
+            {
+                return View();
+            }
+            
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
