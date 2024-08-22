af6113b4e86ac399e4465e2bfcd7737a264eff58
sarzapalo <sarzapalosupanta@gmail.com>
2024-08-21 22:29:02 -0500

    feat: add metodos cruds

--- Controllers/TareasController.cs
+++ Controllers/TareasController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RetoTecnicoAjinomoto.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,101 @@ namespace RetoTecnicoAjinomoto.Controllers
 {
     public class TareasController : Controller
     {
+        // GET: Tareas
         public ActionResult Index()
         {
-            return View();
+            using (TareasDbModels context = new TareasDbModels())
+            {
+                return View(context.Tareas.ToList());
+            }
         }
 
-        public ActionResult About()
+        // GET: Tareas/Details/5
+        public ActionResult Details(int id)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            using(TareasDbModels context = new TareasDbModels())
+            {
+                return View(context.Tareas.Where(x => x.Id == id));
+            }
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
-            return View();
+        // POST: Tareas/Create
+        [HttpPost]
+        public ActionResult Create(Tareas oTareas)
+        {
+            try
+            {
+                using (TareasDbModels context = new TareasDbModels())
+                {
+                    context.Tareas.Add(oTareas);
+                    context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tareas/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (TareasDbModels context = new TareasDbModels())
+            {
+                return View(context.Tareas.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Tareas/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Tareas oTareas)
+        {
+            try
+            {
+                using (TareasDbModels context = new TareasDbModels())
+                {
+                    context.Entry(oTareas).State = System.Data.EntityState.Modified;
+                    context.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Tareas/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (TareasDbModels context = new TareasDbModels())
+            {
+                return View(context.Tareas.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Tareas/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                using (TareasDbModels context = new TareasDbModels())
+                {
+                    Tareas oTareas = context.Tareas.Where(x => x.Id == id).FirstOrDefault();
+                    context.Tareas.Remove(oTareas);
+                    context.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
